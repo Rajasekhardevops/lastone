d51ef272945293bcbe9bd7017c748f1d35bbef20
sekhardevop <rajasekhargaddam456@gmail.com>
2019-05-07 16:26:53 +0530

    1

---
+++ src/test/java/lastone/numbertest.java
@@ -0,0 +1,14 @@
+package lastone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class numbertest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
